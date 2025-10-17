072346444824f93f949f94f1184c35b1ef812090
S <trdhgfukc@gmail.com>
2025-10-17 23:07:09 +0700

    Homework4 final

--- src/exercise3.c
+++ src/exercise3.c
@@ -1,21 +1,31 @@
 #include <stdio.h>
 int main(int argc, char** argv) {
-    float m[10],g[10];
+    float a[10],g[10];
     for (int i = 0; i<10;i++){
         scanf("%f",&g[i]);
     }
     int k;
     scanf("%d", &k);
-    for (int i = 0; i<10; i++){
+    for (int i = 0; i < 10; ++i){
         if (k>0){
-            m[i]=g[(i+9)%10];
+            if (i==0){
+                a[i]=g[9];
+            }
+            else {
+                a[i]=g[i-1];
+            }
         }
-    else{
-        m[i]=g[(i+1)%10];
+        else{
+            if (i==9){
+                a[9]=g[0];
+            }
+            else{
+                a[i]=g[i+1];
+            }
+        }   
     }
     for (int i = 0; i < 10; i++){
-        printf("%.1f", m[i]);
-    }
+        printf("%.2f ", a[i]);
     }
     return 0;
-}
+    }
--- src/exercise4.c
+++ src/exercise4.c
@@ -1,28 +1,29 @@
 #include <stdio.h>
 
 int main(int argc, char** argv) {
-    float m[10];
-    for (int i = 0; i<10; ++i){
-        scanf("%f", &m[i]);
-    }
-    float *g=m;
-    float x;
+    float a[10],g[10];
     int k;
+    float x;
+    for (int i = 0; i < 10; ++i){
+        scanf("%f", &g[i]);  
+    }
     scanf("%f", &x);
     scanf("%d", &k);
-    for (int i = 0; i<10; ++i){
+    for (int i = 0; i < 10; ++i){
         if (i<k){
-            m[i]=g[i];
+            a[i]=g[i];
         }
         else if (i==k){
-            m[i]=x;
+            a[i]=x;
         }
         else if (i>k){
-            m[i]=g[i-1];
+            a[i]=g[i-1];
         }
-    }
+        }
+
+   
     for (int i = 0; i<10; ++i){
-        printf(" %f", m[i]);
+        printf(" %f", a[i]);
     }
     return 0;
 }
