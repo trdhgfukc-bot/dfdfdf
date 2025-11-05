b05ef41e1e9ddaa0aa19fe12ad2454b356e25324
S <trdhgfukc@gmail.com>
2025-11-06 00:44:44 +0700

    Создан конструктор cache

--- Cache.java
+++ Cache.java
@@ -1,5 +1,13 @@
+
+import java.util.LinkedList;
+
 public class Cache<T>{
     private int size;
-    
+    private LinkedList<T> el;
+    public Cache(int size)
+{
+    this.size = size;
+    this.el = new LinkedList<>();
+}
 
 }
