292d7ba9af0ed4bfaee94a63050af6a588073946
S <trdhgfukc@gmail.com>
2025-11-12 21:00:39 +0700

    dz6

--- Car.java
+++ Car.java
@@ -6,6 +6,7 @@ public class Car {
     private String color;
     private double price;
     private String  rgnum;
+
     public Car(int id, String brend, String model, int year, String color, double price, String rgnum){
         this.id = id;
         this.brend = brend;
@@ -15,45 +16,59 @@ public class Car {
         this.price = price;
         this.rgnum =rgnum;
     }
+
     public int getid(){
         return id;
     }
+
     public void setid(int id){
         this.id = id;
     }
+
     public String getbrend(){
         return brend;
     }
+
     public void setbrend(String brend){
         this.brend = brend;
     }
+
     public String getmodel(){
         return model;
     }
+
     public void setmodel(String model){
         this.model = model;
     }
+
     public int getyear(){
         return year;
     }
+
     public void setyear(int year){
         this.year = year;
     }
+
     public String getcolor(){
         return color;
     }
+
     public void setcolor(String color){
         this.color = color;
     }
+
     public double getprice(){
         return price;
     }
+
     public void setprice(double price){
         this.price = price;
     }
+
     public String getrgnum(){
         return rgnum;
     }
+    
     public void setrgnum(String rgnum){
         this.rgnum = rgnum;
     }
--- Main.java
+++ Main.java
@@ -33,4 +33,5 @@ public class Main{
         }
         return cars;
     }
+    
 }
